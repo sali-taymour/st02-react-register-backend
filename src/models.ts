eec9f52ac5c1a6c54ef64e1fc2cec2072956b626
sali <sali.taymor@gmail.com>
2022-08-23 19:45:53 +0200

    refactoring interfaces and models

---
+++ src/models.ts
@@ -0,0 +1,7 @@
+import { IUser } from "./interfaces.js";
+
+ export const user: IUser = {
+    firstName: "Hendrick",
+    lastName: "Denzmann",
+    accessGroups: ["loggedInUsers", "members"],
+};
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
+import {user} from './models.js'
 
 const app = express();
 
 const PORT = process.env.PORT || 3049;
-
 interface IUser {
     firstName: string;
     lastName: string;
     accessGroups: string[];
 }
 
-const user: IUser = {
-    firstName: "Hendrick",
-    lastName: "Denzmann",
-    accessGroups: ["loggedInUsers", "members"],
-};
+
+
 
 app.get("/", (req: express.Request, res: express.Response) => {
     res.send(user);
