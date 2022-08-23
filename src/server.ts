3b73ea74b73f99752ca200c7e7824fe1a02782c0
sali <sali.taymor@gmail.com>
2022-08-23 22:37:32 +0200

    current user  logged in

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
-import {getUsers} from './models.js'
+import { getUsers } from "./models.js";
 import session from "express-session";
 import cookieParser from "cookie-parser";
+
+declare module "express-session" {
+    export interface SessionData {
+        user: { [key: string]: any };
+    }
+}
+
 dotenv.config();
 
-
 const app = express();
-
 const PORT = process.env.PORT || 3049;
-const users = getUsers()
 
+const users = getUsers();
+const loginSecondsMax = 10;
 
-
-app.get("/", (req: express.Request, res: express.Response) => {
-    res.send(users);
-});
 app.use(
     session({
         resave: true,
@@ -23,8 +25,68 @@ app.use(
         secret: "tempsecret",
     })
 );
-
 app.use(cookieParser());
+app.use(express.json());
+
+const logAnonymousUserIn = (req: express.Request, res: express.Response) => {
+    const user = users.find((user) => user.username === "anonymousUser");
+    if (user) {
+        req.session.user = user;
+        req.session.cookie.expires = new Date(
+            Date.now() + loginSecondsMax * 1000
+        );
+        req.session.save();
+        res.send({
+            currentUser: user,
+        });
+    } else {
+        res.status(500).send("bad login");
+    }
+};
+
+const logUserIn = (
+    username: string,
+    req: express.Request,
+    res: express.Response
+) => {
+    let user = users.find((user) => user.username === username);
+    if (user) {
+        req.session.user = user;
+        req.session.cookie.expires = new Date(
+            Date.now() + loginSecondsMax * 1000
+        );
+        req.session.save();
+        res.send({
+            currentUser: user,
+        });
+    } else {
+        logAnonymousUserIn(req, res);
+    }
+};
+
+app.get("/", (req: express.Request, res: express.Response) => {
+    res.send(users);
+});
+
+app.post("/login", (req: express.Request, res: express.Response) => {
+    const username = req.body.username;
+    logUserIn(username, req, res);
+});
+
+app.get("/current-user", (req: express.Request, res: express.Response) => {
+    const user = req.session.user;
+    if (user) {
+        res.send({
+            currentUser: user,
+        });
+    } else {
+        logAnonymousUserIn(req, res);
+    }
+});
+
+app.get("/logout", (req: express.Request, res: express.Response) => {
+    logAnonymousUserIn(req, res);
+});
 
 app.listen(PORT, () => {
     console.log(`listening to API on http://localhost:${PORT}`);
