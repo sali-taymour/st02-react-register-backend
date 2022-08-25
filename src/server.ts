e2460b7b08c07ea53516becb0e9e48b09f74be15
sali <sali.taymor@gmail.com>
2022-08-25 20:01:14 +0200

    added env

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { getUsers } from "./models.js";
+import mongoose from "mongoose";
+import { User } from "./models/User.js";
 import session from "express-session";
 import cookieParser from "cookie-parser";
-
-dotenv.config();
+import bcrypt from "bcrypt";
 
 declare module "express-session" {
     export interface SessionData {
         user: { [key: string]: any };
     }
 }
-
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URI);
 
 const app = express();
 const PORT = process.env.PORT || 3049;
 
-const users = getUsers();
 const loginSecondsMax = 10;
 
 app.use(cookieParser());
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
     cors({
-        origin:  process.env.FRONTEND_BASE_URL,
+        origin: process.env.FRONTEND_BASE_URL,
         methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
         credentials: true,
     })
@@ -43,14 +43,18 @@ app.use(
         },
     })
 );
+app.set("trust proxy", 1);
 
 app.all("/", function (req, res, next) {
     res.header("Access-Control-Allow-Origin", process.env.FRONTEND_BASE_URL);
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
-const logAnonymousUserIn = (req: express.Request, res: express.Response) => {
-    const user = users.find((user) => user.username === "anonymousUser");
+const logAnonymousUserIn = async (
+    req: express.Request,
+    res: express.Response
+) => {
+    const user = await User.findOne({ username: "anonymousUser" });
     if (user) {
         req.session.user = user;
         req.session.cookie.expires = new Date(
@@ -65,38 +69,43 @@ const logAnonymousUserIn = (req: express.Request, res: express.Response) => {
     }
 };
 
-const logUserIn = (
+const logUserIn = async (
     username: string,
+    password: string,
     req: express.Request,
     res: express.Response
 ) => {
-    let user = users.find((user) => user.username === username);
+    const user = await User.findOne({ username });
     if (user) {
-        req.session.user = user;
-        req.session.cookie.expires = new Date(
-            Date.now() + loginSecondsMax * 1000
-        );
-        req.session.save();
-        res.send({
-            currentUser: user,
-        });
+        const passwordIsCorrect = await bcrypt.compare(password, user.hash);
+        if (passwordIsCorrect) {
+            req.session.user = user;
+            req.session.cookie.expires = new Date(
+                Date.now() + loginSecondsMax * 1000
+            );
+            req.session.save();
+            res.send({
+                currentUser: user,
+            });
+        } else {
+            logAnonymousUserIn(req, res);
+        }
     } else {
         logAnonymousUserIn(req, res);
     }
 };
+
 app.get("/", (req: express.Request, res: express.Response) => {
     res.send(`***${process.env.NODE_ENV}***`);
 });
+app.post("/login", (req: express.Request, res: express.Response) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    logUserIn(username, password, req, res);
+});
 
 
-app.set('trust proxy', 1);
- 
-origin: process.env.FRONTEND_BASE_URL,
 
-    app.post("/login", (req: express.Request, res: express.Response) => {
-        const username = req.body.username;
-        logUserIn(username, req, res);
-    });
 
 app.get("/current-user", (req: express.Request, res: express.Response) => {
     const user = req.session.user;
@@ -108,7 +117,6 @@ app.get("/current-user", (req: express.Request, res: express.Response) => {
         logAnonymousUserIn(req, res);
     }
 });
-
 
 app.get("/logout", (req: express.Request, res: express.Response) => {
     logAnonymousUserIn(req, res);
